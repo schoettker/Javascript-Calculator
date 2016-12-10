9d561473ac37d200b3c820e0bb25ba922c90d8b8
schoettker <lennartschoettker@hotmail.com>
2016-12-10 15:30:05 +0100

    replace . with , and / with obelus; fix issue where after a computation when a num was clicked it was appended to the old result in string form instead of starting a new calculation

--- script.js
+++ script.js
@@ -4,15 +4,21 @@ window.onload = function() {
 }
 
 var str = new String();
+var toggleReset = false;
 function addNumsEventListeners() {
   var nums = document.getElementById('numblock').childNodes;
   for (var i = 0, l = nums.length - 2; i < l; i++) {
     var num = nums[i];
     num.addEventListener('click', function(e) {
       // console.log(this.getAttribute('data-value'));
+      if (toggleReset) {
+        str = '';
+        document.getElementById('input-history').innerHTML = null;
+        toggleReset = false;
+      }
       str += this.getAttribute('data-value');
       display(this.getAttribute('data-value'), 'input-current');
-      display(str, 'input-history');
+      display(str, 'input-current');
       console.log(str);
     });
   }
@@ -22,11 +28,14 @@ function addOperandsEventListeners() {
   for (var i = 0, l = operands.length; i < l; i++) {
     var operand = operands[i];
     operand.addEventListener('click', function(e) {
-      if (!isNaN(str[str.length-1])) {
-        str += this.getAttribute('data-operation');
-        display(str, 'input-history');
-        console.log(str);
+      // if (!isNaN(str[str.length-1])) {
+      if (isNaN(str[str.length-1])) {
+        str = str.slice(0, str.length - 1);
       }
+      str += this.getAttribute('data-operation');
+      display(str, 'input-current');
+      toggleReset = false;
+      // }
     })
   }
 }
@@ -40,12 +49,18 @@ function compute() {
       // display(eval(str), 'input-current');
       str = String(eval(str));
       console.log(str);
-      display(eval(str), 'input-current');
+      display(eval(str), 'input-history');
+      toggleReset = true;
     });
 }
 compute();
 function display(input, parent) {
+  // var regex  = new RegExp('')
   document.getElementById(parent).innerHTML = null;
+  if (typeof input == 'string') {
+    input = input.replace(/\//g, '÷');
+    input = input.replace(/\./g, '\,');
+  }
   document.getElementById(parent).
     appendChild(document.createTextNode(input));
 }
