2a79fa0428e034c7dbea7d0d42c5e6dd70b4652f
schoettker <lennartschoettker@hotmail.com>
2016-12-10 12:35:26 +0100

    display current input, input history and show result

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ function addNumsEventListeners() {
     num.addEventListener('click', function(e) {
       // console.log(this.getAttribute('data-value'));
       str += this.getAttribute('data-value');
+      display(this.getAttribute('data-value'), 'input-current');
+      display(str, 'input-history');
       console.log(str);
     });
   }
@@ -22,8 +24,7 @@ function addOperandsEventListeners() {
     operand.addEventListener('click', function(e) {
       if (!isNaN(str[str.length-1])) {
         str += this.getAttribute('data-operation');
-        console.log(str);
-      } else {
+        display(str, 'input-history');
         console.log(str);
       }
     })
@@ -32,7 +33,19 @@ function addOperandsEventListeners() {
 function compute() {
   document.querySelector('div[data-value="compute"]')
     .addEventListener('click', function(event) {
-      console.log(eval(str));
+      if (isNaN(str[str.length-1])) {
+        str = str.slice(0, str.length - 1);
+      }
+      // console.log(eval(str));
+      // display(eval(str), 'input-current');
+      str = String(eval(str));
+      console.log(str);
+      display(eval(str), 'input-current');
     });
 }
 compute();
+function display(input, parent) {
+  document.getElementById(parent).innerHTML = null;
+  document.getElementById(parent).
+    appendChild(document.createTextNode(input));
+}
